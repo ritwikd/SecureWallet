f36785fc1bc3029f9e98afbb493ad238457b10dc
Ritwik Dutta <ritzymail@gmail.com>
2014-02-18 17:09:43 -0800

    Added the file. It creates its configuration in the home directory of the user.

---
+++ securewallet.py
@@ -0,0 +1,66 @@
+from os import listdir, urandom, system
+from os.path import expanduser
+from Crypto.Cipher import AES
+from hashlib import sha256
+from getpass import getpass
+
+done = False
+userPass = ""
+hashedPass = ""
+helpData = """H - Display the help Page\n\
+X - Exit from the thing\n\
+C - Clear the screen\n\
+A - Add an item to the screen\n\
+"""
+
+
+if (expanduser("~/.securewallet") in listdir(".")):
+	dataHandler = open(expanduser("~/.securewallet"), "r")
+	
+	hashedPass = dataHandler.read(32)
+	dataHandler.close()
+	ind = 0;
+	while (userPass != hashedPass):
+		if (ind > 2):
+			print "You have entered an incorrect password 3 times."
+			exit()
+		else:
+			userPass = getpass("Enter your password (" + str(3-ind) + " tries):")
+			if (sha256(userPass).digest() == hashedPass):
+				break
+				print "Logged in successfully."
+				system("clear")
+			else:
+				print "Incorrect password."
+				ind += 1
+else:
+	same = False
+	while not same:
+		userPass = getpass("Enter a password for your data:")
+		userPass2 = getpass("Confirm password:")
+		if (userPass == userPass2):
+			same = True
+		else:
+			print "Passwords did not match."
+
+	dataHandler = open(expanduser("~/.securewallet"), "w+")
+	dataHandler.write(sha256(userPass).digest())
+	dataHandler.close()
+
+
+
+while not done:
+	userInput = raw_input("Enter a command (H for help):").lower()
+	if (userInput in ["h", "help"]):
+		print helpData
+	elif (userInput in ["x", "exit"]):
+		print "Exiting program."
+		done = True
+	elif (userInput in ["c", "clear"]):
+		system("clear")
+	elif (userInput in ["a", "add", "n", "new"]):
+		print "Adding element:"
+	else:
+		print "Unrecognized command."
+
+exit()
