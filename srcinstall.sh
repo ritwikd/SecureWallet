f2bf0cf67454be39dd26741069899822b8f1c3de
ritzbitzn <ritzymail@gmail.com>
2014-02-22 17:52:25 -0800

    F

---
+++ srcinstall.sh
@@ -0,0 +1,8 @@
+sudo curl http://online.ritwikd.com/ritwik/securewallet/raw/master/securewallet.py -o securewallet.py
+echo "Finished download."
+mv securewallet.py /usr/bin/securewallet.py
+echo "Moved file to /usr/bin."
+echo 'alias securewallet="./usr/bin/securewallet.py"' >> ~/.bash_profile
+echo 'alias securewallet="./usr/bin/securewallet.py"' >> ~/.bashrc
+echo 'alias securewallet="./usr/bin/securewallet.py"' >> ~/.zshrc
+echo "Added the aliases. Please reopen shell."
