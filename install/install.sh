ec8aeb6782a1565ae16b1d4c7e6861d0ff4bf1a4
ritzbitzn <ritzymail@gmail.com>
2014-02-22 18:10:59 -0800

    Scripts

--- install/install.sh
+++ install/install.sh
@@ -5,9 +5,9 @@ echo "Moved file to /usr/bin."
 sudo echo 'alias securewallet="./usr/bin/securewallet"' >> ~/.bash_profile
 sudo echo 'alias securewallet="./usr/bin/securewallet"' >> ~/.bashrc
 sudo echo 'alias securewallet="./usr/bin/securewallet"' >> ~/.zshrc
-sudo echo 'sudo export PATH=$PATH:/usr/local/sbin:/usr/local/bin' >> ~/.bash_profile
-sudo echo 'sudo export PATH=$PATH:/usr/local/sbin:/usr/local/bin' >> ~/.bashrc
-sudo echo 'sudo export PATH=$PATH:/usr/local/sbin:/usr/local/bin' >> ~/.zshrc
+sudo echo 'export PATH=$PATH:/usr/local/sbin:/usr/local/bin' >> ~/.bash_profile
+sudo echo 'export PATH=$PATH:/usr/local/sbin:/usr/local/bin' >> ~/.bashrc
+sudo echo 'export PATH=$PATH:/usr/local/sbin:/usr/local/bin' >> ~/.zshrc
 sudo chmod +x /usr/bin/securewallet
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin
 echo "Added the aliases. Please reopen shell."
