edcecf5035fe00ea88d0248bf75c53860cdfc72b
ritzbitzn <ritzymail@gmail.com>
2014-02-22 17:24:48 -0800

    Added installer script.

--- pyinstall.sh
+++ pyinstall.sh
@@ -1,7 +1,8 @@
-curl -o	http://online.ritwikd.com/ritwik/securewallet/raw/master/securewallet.py securewallet.py
+curl http://online.ritwikd.com/ritwik/securewallet/raw/master/securewallet.py -o securewallet.py
 echo "Finished download."
 mv securewallet.py /usr/bin/securewallet.py
 echo "Moved file to /usr/bin."
-echo 'securewallet = "python /usr/bin/securewallet.py"' >> ~/.bash_profile
-echo 'securewallet = "python /usr/bin/securewallet.py"' >> ~/.bashrc
-echo "Added the aliases."
+echo 'alias securewallet="python /usr/bin/securewallet.py"' >> ~/.bash_profile
+echo 'alias securewallet="python /usr/bin/securewallet.py"' >> ~/.bashrc
+echo 'alias securewallet="python /usr/bin/securewallet.py"' >> ~/.zshrc
+echo "Added the aliases. Please reopen shell."
--- securewallet.py
+++ securewallet.py
@@ -25,12 +25,15 @@ if (".securewallet" in listdir(expanduser("~"))):
 	dataHandler = open(expanduser("~/.securewallet"), "rb")
 	hashedPass = dataHandler.read(32)
 	ind = 0;
+
 	while (userPass != hashedPass):
 		if (ind > 2):
 			print "You have entered an incorrect password 3 times."
 			exit()
+
 		else:
 			userPass = getpass("Enter your password (" + str(3-ind) + " tries):")
+
 			if (sha256(userPass).digest() == hashedPass):	
 				print "Logged in successfully."
 				randomIV = dataHandler.read(16)
@@ -40,24 +43,31 @@ if (".securewallet" in listdir(expanduser("~"))):
 					splLine = line.split('\xbe')
 					userInfo[splLine[0]] = splLine[1:]
 				dataHandler.close()
+
 				if (len(userInfo) > 0):
 					print "Loaded elements successfully."
 					print "Listing elements."
 					for i in range(len(userInfo.keys())):
-						print str(i+1) + ". "  + userInfo.keys()[i]		
+						print str(i+1) + ". "  + userInfo.keys()[i]
+
 				else:
 					print "No account data in file."			
 				break
+
 			else:
 				print "Incorrect password."
 				ind += 1
+
 else:
 	same = False
+
 	while not same:
 		userPass = getpass("Enter a password for your data:")
 		userPass2 = getpass("Confirm password:")
+
 		if (userPass == userPass2):
 			same = True
+
 		else:
 			print "Passwords did not match."
 
@@ -69,6 +79,7 @@ else:
 
 while not done:
 	userInput = raw_input("Enter a command (H for help):").lower()
+
 	if (userInput in ["h", "help"]):
 		print helpData
 
@@ -87,6 +98,7 @@ while not done:
 		randomIV = urandom(16)
 		dataHandler.write(randomIV)
 		plainText = ""
+		
 		for key in userInfo.keys():
 			plainText = plainText + '\xbe'.join([key, userInfo[key][0], userInfo[key][1]]) + '\n'
 		cipherData = AES.new(sha256(userPass).digest(), AES.MODE_CFB, randomIV).encrypt(plainText)
@@ -94,10 +106,13 @@ while not done:
 		dataHandler.close()
 
 	elif (userInput in ["l", "list", "s", "show"]):
+
 		if (len(userInfo) > 0):
 			print "Listing elements."
+
 			for i in range(len(userInfo.keys())):
 				print str(i+1) + ". "  + userInfo.keys()[i]
+
 		else:
 			print "No elements to list."
 
@@ -112,16 +127,20 @@ while not done:
 			print "=" * len(userInfo.keys()[userInput-1])
 			print "Username:" + userInfo[userInfo.keys()[userInput-1]][0]
 			print "Password:" + userInfo[userInfo.keys()[userInput-1]][1]
+
 		else:
 			print "No items to view."
 
 	elif (userInput in ["d", "delete", "r", "remove"]):
+
 		if (len(userInfo) > 0):
 			print "Listing elements."
+
 			for i in range(len(userInfo.keys())):
 				print str(i+1) + ". "  + userInfo.keys()[i]
 			userInput = input("Choose an item to view: ")
 			del(userInfo[userInfo.keys()[userInput-1]])
+
 		else:
 			print "No items to delete."
 	
