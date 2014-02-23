af75e580e862223f860b81bcb83e34a73defcfcc
ritzbitzn <ritzymail@gmail.com>
2014-02-22 17:55:05 -0800

    F

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,4 @@
-su
-curl http://online.ritwikd.com/ritwik/securewallet/raw/master/securewallet -o securewallet
+sudo curl http://online.ritwikd.com/ritwik/securewallet/raw/master/securewallet -o securewallet
 echo "Finished download."
 mv securewallet /usr/bin/securewallet
 echo "Moved file to /usr/bin."
